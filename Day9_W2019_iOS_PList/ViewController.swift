bebbccf92a24899fcb6066424cbe8d8b362b00c2
MacStudent <macstudent@126-iMac05.local>
2019-03-12 19:05:45 -0400

    Initial Commit

---
+++ Day9_W2019_iOS_PList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day9_W2019_iOS_PList
+//
+//  Created by MacStudent on 2019-03-12.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
